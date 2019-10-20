7b35e7319745bc9339e19e381a07296de9dfe1ff
Steve Bradley <steve@debian8svm.debian8svm>
2019-10-20 15:50:08 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="myce",
+    version="0.0.1",
+    description="API definitions and client",
+    url="https://github.com/rotwatsb/myce",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
