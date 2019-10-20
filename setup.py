5e50614bdb4de4eb7b3f5e7f2bf8a846555c1ad4
Steve Bradley <steve@debian8svm.debian8svm>
2019-10-20 18:39:40 -0400

    add readme and license

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="myce",
     version="0.0.1",
+    author="Steve Bradly",
+    author_email="stevebradley@gmail.com",
     description="API definitions and client",
-    url="git://github.com:rotwatsb/myce.git",
-    author="Steve Bradley",
-    packages=["myce"],
-    license="unlicensed",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rotwatsb/myce",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires='>=3.6',
 )
