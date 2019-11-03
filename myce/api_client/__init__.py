8eeca810ed6cda03797665b2e76baf2fb7811a8c
Steve Bradley <steve@debian8svm.debian8svm>
2019-11-03 15:56:10 -0500

    add user create and get endpoints

--- myce/api_client/__init__.py
+++ myce/api_client/__init__.py
@@ -25,34 +25,36 @@ class APIClient(APIClientNode):
         
     def __build_client(self, api):
         for resource in api.resources:
-            node_path = resource.raw.get('get').get('(client_name)').split('.')
+            node_path = resource.raw[resource.method]['(client_name)'].split('.')
             client_node = self.get_client_node(node_path)
             resource_function = self.__build_resource_function(resource);
 
             setattr(client_node, node_path[-1], resource_function)
 
     def __build_resource_function(self, resource):
-        implementer = resource.raw.get('get').get('(implementer)')
+        implementer = resource.raw[resource.method]['(implementer)']
         base_uri = '%s:%s'%(config[implementer]['domain'], config[implementer]['port'])
         uri_params = resource.uri_params
 
+        # as a side effect, this function removes uri params from the params object
         def interpolatePath(path, params):
             if uri_params is not None:
                 for uri_param in uri_params:
                     path = path.replace('{%s}' %uri_param.name, params[uri_param.name])
+                    del params[uri_param.name]
 
             return path
                 
         def f(**kwargs):
             interpolated_uri = 'http://' + base_uri + interpolatePath(resource.path, kwargs)
             http_method = getattr(requests, resource.method)
-            
-            return http_method(interpolated_uri)
+
+            if http_method is 'get':
+                return http_method(interpolated_uri)
+            elif http_method is 'post':
+                return http_method(interpolated_uri, json=kwargs)
 
         return f
-
-
-    
 
 
 
